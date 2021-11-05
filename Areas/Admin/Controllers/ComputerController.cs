e83777011285926d665e4e04998f4a1d601396f1
Hrisikesh Kumar <Hrisikesh.kumar@icsi.edu>
2021-11-05 18:14:05 +0530

    ok

---
+++ Areas/Admin/Controllers/ComputerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_Hardware_Aug2021.Areas.Admin.Controllers
+{
+    public class ComputerController : Controller
+    {
+        // GET: Admin/Computer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submit()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Areas/Admin/Controllers/LaptopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IT_Hardware_Aug2021.Areas.Admin.Controllers
+{
+    public class LaptopController : Controller
+    {
+        // GET: Admin/Laptop
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
